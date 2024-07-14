e1c9c5e6ffa9c4b1732521c94a20b6dc0cc513f0
MaciejSantkiewicz <111911254+MaciejSantkiewicz@users.noreply.github.com>
2024-07-14 11:05:36 +0200

    Update db_table_creation_code.sql
    fixing spelling

--- db_table_creation_code.sql
+++ db_table_creation_code.sql
@@ -4,13 +4,13 @@ use twitter_x;
 create table users(
 	id int auto_increment primary key, 
     user_name varchar(255) unique not null,
-    create_at timestamp	default now()
+    created_at timestamp	default now()
     );
 
 create table tweets(
 	id int auto_increment primary key, 
     tweet_text varchar(255) not null,
-    create_at timestamp	default now(),
+    created_at timestamp	default now(),
     user_id int not null,
     
     foreign key(user_id) references users(id)
@@ -28,7 +28,7 @@ create table likes(
     
 create table followers(
 	follower_id int not null,
-    followee_id int not null,
+	followee_id int not null,
     
     foreign key(follower_id) references users(id),
     foreign key(followee_id) references users(id),
