6f89cf4a204b0551898b561bba70b07f4305044b
MaciejSantkiewicz <111911254+MaciejSantkiewicz@users.noreply.github.com>
2024-08-17 13:53:54 +0200

    Update db_table_creation_code.sql

--- db_table_creation_code.sql
+++ db_table_creation_code.sql
@@ -29,6 +29,7 @@ create table likes(
 create table followers(
 	follower_id int not null,
 	followee_id int not null,
+	created_at timestamp default now(),
     
     foreign key(follower_id) references users(id),
     foreign key(followee_id) references users(id),
